c032a2e370527e4c8829653ef018f5ab12bda4ed
Elina Dadakhanova <dadakhan@sheridancollege.ca>
2024-01-25 13:46:42 -0500

    1

--- Week3/main.cpp
+++ Week3/main.cpp
@@ -1,10 +1,16 @@
 // Week3.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "FileController.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	FileController* fc = &FileController::Instance();
+	cout << fc->GetCurDirectory() << endl;
+	int fs = fc->GetFileSize("FileController.cpp");
+	unsigned char* buffer = new unsigned char[fs];
+	if (fc->ReadFile("FileController.cpp", buffer, fs)) {
+		cout << "File read operation successful." << endl;
+	}
+	delete[] buffer;
 }
-
